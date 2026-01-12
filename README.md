19fc40e14bb44c80ac69267429340517b5125e88
Raghav893 <raghavarora2309@gmail.com>
2026-01-12 16:08:52 +0530

    created some grep commands and also updated readme .md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 This REPO IS TO TRACK AND MAKE NOTES AND LEARN MY BASICS OF LINUX 
+
+# TO PRACTICE IT
+- create aws account
+- launch an ec2 instance
+- select OS as any linux os you like
+- connect your git bash using ssh 
---
+++ grep-commands.sh
@@ -0,0 +1,6 @@
+# To get lines from a file where it contains a certain word like sample then use
+grep "sample" sample.txt
+#to ignore case sensitivity
+grep  -i "sample" sample.txt
+# to get lines where it do not contain a certian word lets sample then 
+grep -v "sample" sample.txt
