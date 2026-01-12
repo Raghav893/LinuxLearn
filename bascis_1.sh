a9f9f432b172c7b52088449487ff17c67a4fa259
Raghav893 <raghavarora2309@gmail.com>
2026-01-12 15:08:57 +0530

    basics 1 completed

--- bascis_1.sh
+++ bascis_1.sh
@@ -25,3 +25,21 @@ rmdir mypc
 
 cd newWkdir
 
+#to get back to previous directory or out of the folder 
+cd ..
+
+#If we try to remove a directory having some data it is not possible a directory has to be empty before deleted
+# But to force dlt the directory command is 
+rm -rf directoryName
+
+#To display your all files and sort in them in latest time created 
+ls -lt
+#To reverse it 
+ls -ltr
+#or
+ls -lr
+
+#to delete all files with a certain extension example .txt then 
+rm *.txt 
+
+
