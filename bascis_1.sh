fbde86df3ec923fb08fb37b6ea9b168cb82dad72
Raghav893 <raghavarora2309@gmail.com>
2026-01-12 14:46:14 +0530

    new

--- bascis_1.sh
+++ bascis_1.sh
@@ -10,6 +10,7 @@ ls
 ls -l
 
 #if a file starts with d it means its a directory file(folder)
+#if a file starts with - it means its a  file
 
 # To create a new file command should be
 
@@ -17,5 +18,10 @@ touch linux.txt
 #or
 touch linux.yml
 
+#remove a  directory we use 
+rmdir mypc
 
+#to change your current working directry
+
+cd newWkdir
 
