bcaec7adfedfaab87d542eee46461631ce2dbdcb
Raghav893 <raghavarora2309@gmail.com>
2026-01-12 14:37:03 +0530

    making a new directory and creating files

---
+++ filesystem.sh
@@ -0,0 +1,21 @@
+#CURRENT WORKING DIRECTORy
+pwd
+
+#make directory
+mkdir directoryname
+
+#To look in your current working directory
+ls
+#
+ls -l
+
+#if a file starts with d it means its a directory file(folder)
+
+# To create a new file command should be
+
+touch linux.txt
+#or
+touch linux.yml
+
+
+
