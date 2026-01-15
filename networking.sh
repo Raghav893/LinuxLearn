8e24409cdfd984c172d06acd883716f78b1ce739
Raghav893 <raghavarora2309@gmail.com>
2026-01-16 00:08:51 +0530

    installing git int linux

---
+++ networking.sh
@@ -0,0 +1,8 @@
+# To download any software or access any rest api or HTTP server Networking commands are used
+#to download something we need super user privellege which is get through sudo command 
+# to download any software in amazon linux or redhar we uses yum and in ubuntu we use apt
+
+#FOR EXAMPLE
+
+sudo yum install git #This will install git and you can check it using 
+git -v
