05dae125d7deb8c786d9766ce134ca50fee518af
Raghav893 <raghavarora2309@gmail.com>
2026-01-16 00:56:45 +0530

    done

--- FindAndLocate.sh
+++ FindAndLocate.sh
@@ -1,3 +1,17 @@
 # Find and locate are used to find a file in a directory 
 #We have to install locate first by 
 sudo dnf install locate
+
+#locate searches for file in its own db to update its db we have to run a command as
+sudo updatedb
+#after this write 
+locate aws.txt
+#This will return the path of the file location 
+
+#USING OF FIND
+find -name aws.txt #THIS WILL GIVe PATH
+
+#TO search in entire linux 
+find /home -name aws.txt
+
+#LOCATE IS FASTER BUT WE NEED TO UPDATE IT REGULARLY
